1b41b91c85507cef5c59e71e2aed4e8559a5322f
Pankil Joshi <pankil@zeroek.in>
2020-10-04 19:03:04 +0530

    Added filter to fetch service url

--- src/Liquid/Filters/URL.php
+++ src/Liquid/Filters/URL.php
@@ -8,7 +8,9 @@
 
 namespace Liquid\Filters;
 
+use App\Helpers\Helper;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL as FacadesURL;
 
 class URL
 {
@@ -19,9 +21,22 @@ class URL
      *
      * @return string
      */
-    public static function asset_url($input)
+    public static function asset_url(string $input): string
     {
         return Config::get('app.asset_url') . $input;
+    }
+
+    /**
+     * Return asset URL
+     *
+     * @param string $input
+     *
+     * @return string
+     */
+    public static function service_url(int $id): string
+    {
+        $slug = Helper::getCatalogItemSlug($id);
+        return FacadesURL::route('service.show', [$slug]);
     }
 
 }
